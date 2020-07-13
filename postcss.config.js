11b9776083bd0811fafee3d17f32a4dc7e5a7c0a
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2020-07-13 23:34:19 +0200

    will fix that

--- postcss.config.js
+++ postcss.config.js
@@ -1,17 +1,6 @@
-const purgecss = require("@fullhuman/postcss-purgecss")({
-  content: [
-    "./public/**/*.html",
-    "./src/**/*.js",
-    "./src/**/*.ts",
-    "./src/**/*.tsx"
-  ],
-  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
-});
-
 module.exports = {
-  plugins: [
-    require("tailwindcss"),
-    require("autoprefixer"),
-    ...(process.env.NODE_ENV === "production" ? [purgecss] : [])
-  ]
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 };
