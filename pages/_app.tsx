342b888dc5c3be8b62002ee4d93030a728e2303f
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2021-06-05 21:58:34 +0200

    build main pages

---
+++ pages/_app.tsx
@@ -0,0 +1,20 @@
+import { useState } from 'react';
+import { AppProps } from 'next/app';
+import { ThemeProvider } from 'styled-components';
+import '@shared/tailwind.generated.css';
+import { lightTheme, darkTheme, GlobalStyle } from '../shared/GlobalStyle';
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  const [theme, setTheme] = useState('light');
+
+  const toggleTheme = () => {
+    theme === 'light' ? setTheme('dark') : setTheme('light');
+  };
+
+  return (
+    <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
+}
