9c6444c91b29e8ec7267a8eb8e32acb2f7198a03
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2021-06-06 00:22:18 +0200

    added ga tracking and logrocket

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
 import { useState } from 'react';
 import { AppProps } from 'next/app';
+import LogRocket from 'logrocket';
+import ReactGa from 'react-ga';
 import { ThemeProvider } from 'styled-components';
 import '@shared/tailwind.generated.css';
 import { lightTheme, darkTheme, GlobalStyle } from '../shared/GlobalStyle';
+require('dotenv').config();
+
+LogRocket.init(`${process.env.REACT_APP_LOG_ROCKET_ID}/mannueferreiracom`);
+ReactGa.initialize(`${process.env.REACT_APP_GOOGLE_ANALYTICS}`);
+ReactGa.pageview(window.location.pathname + window.location.search);
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   const [theme, setTheme] = useState('light');
