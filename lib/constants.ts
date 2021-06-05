0340a9062bee2081fb4c070be9532bfd21ef8e49
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2021-06-05 21:59:31 +0200

    add in the util functions

---
+++ lib/constants.ts
@@ -0,0 +1,3 @@
+export const EXAMPLE_PATH = 'mannuelferreira_nextjs'
+export const CMS_NAME = 'Mannuel Ferreira'
+export const HOME_OG_IMAGE_URL = ''
---
+++ lib/logRocket.ts
@@ -0,0 +1,10 @@
+import LogRocket from 'logrocket'
+
+LogRocket.identify(
+  `${process.env.REACT_APP_LOG_ROCKET_ID}/mannuelferreiracom`,
+  {
+    name: "Mannuel Ferreira",
+    email: "mannuel@themwebs.me",
+    subscriptionType: "free"
+  }
+);
