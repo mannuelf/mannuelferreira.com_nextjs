6f97ae50435cd86fbc1ce03c395e2bfaed8fc7f4
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2021-06-07 21:29:38 +0200

    adding google analytics

--- components/meta.tsx
+++ components/meta.tsx
@@ -42,6 +42,23 @@ const Meta = () => {
         href="https://fonts.googleapis.com/css2?family=Fira+Sans:wght@400;500&family=Ubuntu:wght@400;500;700&display=swap"
         rel="stylesheet"
       />
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <script
+        async
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
     </Head>
   );
 };
