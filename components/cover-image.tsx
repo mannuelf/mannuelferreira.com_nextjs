1f9cc64522d677e4265b1b4f87e671a2b24bd71d
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2021-06-17 11:17:05 +0200

    build: use Image not img

--- components/cover-image.tsx
+++ components/cover-image.tsx
@@ -1,5 +1,6 @@
 import cn from 'classnames';
 import Link from 'next/link';
+import Image from 'next/image';
 
 type Props = {
   title: string;
@@ -9,12 +10,13 @@ type Props = {
 
 const CoverImage = ({ title, src, slug }: Props) => {
   const image = (
-    <img
+    <Image
       src={src}
-      alt={`Cover Image for ${title}`}
+      alt={`${title}`}
       className={cn('shadow-small', {
         'hover:shadow-medium transition-shadow duration-200': slug,
       })}
+      layout='fill'
     />
   );
   return (
