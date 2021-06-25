977dd4305d0e13c420cf798cb1da31b216b6e405
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2021-06-25 10:42:52 +0200

    buld: add meta tags component for slugs page

---
+++ components/meta-tags.tsx
@@ -0,0 +1,39 @@
+import Head from 'next/head';
+
+type Props = {
+  ogImage: string;
+  ogSiteName: string;
+  ogTitle: string;
+  ogDescription: string;
+  ogTwitterCard: string;
+  ogTwitterSite: string;
+  ogTwitterCreator: string;
+  ogUrl: string;
+};
+
+export default function MetaTags({
+  ogImage,
+  ogSiteName,
+  ogTitle,
+  ogDescription,
+  ogTwitterCard,
+  ogTwitterCreator,
+  ogTwitterSite,
+  ogUrl,
+}: Props) {
+  return (
+    <Head>
+      <title>
+        {ogTitle} | {ogSiteName}
+      </title>
+      <meta property='og:image' content={ogImage} key='ogimage' />
+      <meta property='og:site_name' content={ogSiteName} key='ogsitename' />
+      <meta property='og:title' content={ogTitle} />
+      <meta property='og:description' content={ogDescription} key='ogdesc' />
+      <meta name='twitter:card' content={ogTwitterCard} />
+      <meta name='twitter:site' content={ogTwitterSite} />
+      <meta name='twitter:creator' content={ogTwitterCreator} />
+      <meta property='og:url' content={ogUrl} key='ogurl' />
+    </Head>
+  );
+}
