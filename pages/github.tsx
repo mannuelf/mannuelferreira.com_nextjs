db19409921444e406f6293ed0bb9af2d7e8203c9
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2021-06-26 13:33:50 +0200

    build: github GraphQL API connection, integration

---
+++ pages/github.tsx
@@ -0,0 +1,77 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import Layout from '@components/Layout/layout';
+import Container from '@components/container';
+import github from '@lib/api/github';
+
+type UserProps = {
+  user: {
+    name: string;
+    repositories: {
+      nodes: [];
+    };
+  };
+};
+
+function GitHub({ user }: UserProps) {
+  const [userName, setUserName] = useState('');
+  const [repoList, setRepoList] = useState(null);
+
+  useEffect(() => {
+    setUserName(user.name);
+    setRepoList(user.repositories.nodes);
+  }, []);
+
+  return (
+    <>
+      <Layout>
+        <Container>hello {userName}</Container>
+      </Layout>
+    </>
+  );
+}
+
+export default GitHub;
+
+export async function getStaticProps() {
+  let user;
+
+  const githubQuery = {
+    query: `
+    {
+      viewer {
+        name
+        repositories(first: 10) {
+          nodes {
+            name
+            description
+            id
+            url
+          }
+        }
+      }
+    }
+    `,
+  };
+
+  try {
+    const res = await axios({
+      url: github.baseUrl,
+      method: 'post',
+      headers: {
+        'content-type': 'application/json',
+        Authorization: github.token,
+      },
+      data: JSON.stringify(githubQuery),
+    });
+    user = res.data.data.viewer;
+  } catch (error) {
+    console.log('🚨', error);
+  }
+
+  return {
+    props: {
+      user,
+    },
+  };
+}
