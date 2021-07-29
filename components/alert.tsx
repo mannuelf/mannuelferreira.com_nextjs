9789cb824b2260e303d677eb6a8f731b44cfebb4
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2021-07-29 12:29:59 +0200

    copy changes

--- components/alert.tsx
+++ components/alert.tsx
@@ -1,6 +1,6 @@
 import Container from './container';
 import cn from 'classnames';
-import { EXAMPLE_PATH } from '@lib/constants';
+import { GITHUB_URL } from '@lib/constants';
 
 type Props = {
   preview?: boolean;
@@ -16,29 +16,14 @@ const Alert = ({ preview }: Props) => {
     >
       <Container>
         <div className='py-2 text-center text-sm'>
-          {preview ? (
-            <>
-              This page is a preview.{' '}
-              <a
-                href='/api/exit-preview'
-                className='underline hover:text-cyan duration-200 transition-colors'
-              >
-                Click here
-              </a>{' '}
-              to exit preview mode.
-            </>
-          ) : (
-            <>
-              The source code for this blog is{' '}
-              <a
-                href={`https://github.com/vercel/next.js/tree/canary/examples/${EXAMPLE_PATH}`}
-                className='underline hover:text-success duration-200 transition-colors'
-              >
-                available on GitHub
-              </a>
-              .
-            </>
-          )}
+          The source code for this blog is{' '}
+          <a
+            href={`${GITHUB_URL}`}
+            className='underline hover:text-success duration-200 transition-colors'
+          >
+            available on GitHub
+          </a>
+          .
         </div>
       </Container>
     </div>
