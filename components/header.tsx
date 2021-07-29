131ea43307fd8f21301d7f5929d0bfef1bf615a6
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2021-07-29 12:30:33 +0200

    style: no more sticky header, it was rubbish ux

--- components/header.tsx
+++ components/header.tsx
@@ -3,7 +3,7 @@ import Logo from '@components/Logo';
 import Container from '@components/container';
 
 const Header = () => (
-  <header className='bg-purple sticky top-0 flex-none flex z-10'>
+  <header className='bg-purple top-0 flex-none flex z-10'>
     <Container>
       <Logo />
     </Container>
