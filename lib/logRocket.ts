989adbabf2d01a18108fdae5d2ff2315b46846f4
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2021-08-03 22:46:40 +0200

    build: clean up

--- lib/logRocket.ts
+++ lib/logRocket.ts
@@ -1,10 +1,11 @@
+import { CMS_NAME, EMAIL } from '@shared/constants';
 import LogRocket from 'logrocket';
 
 LogRocket.identify(
   `${process.env.NEXT_PUBLIC_LOG_ROCKET_ID}/mannuelferreiracom`,
   {
-    name: 'Mannuel Ferreira',
-    email: 'mannuel@themwebs.me',
+    name: `${CMS_NAME}`,
+    email: `${EMAIL}`,
     subscriptionType: 'free',
   },
 );
