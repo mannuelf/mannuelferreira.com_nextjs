adc39939c0b5eafb45521d1c79cf32a322bffe6d
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2021-08-03 22:45:04 +0200

    testing: add cleanup after tests

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,3 +1,4 @@
+import { cleanup } from '@testing-library/react';
 import { server } from './mocks/server';
 
 // Establish API mocking before all tests.
@@ -7,10 +8,12 @@ beforeAll(() => {
 
 // Reset request handlers during tests, so to not affect other tests
 afterEach(() => {
+  cleanup();
   server.resetHandlers();
 });
 
 // clean up after the tests finish
 afterAll(() => {
+  cleanup();
   server.close();
 });
