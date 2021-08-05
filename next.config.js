7dbe3fd1456e8c8e169baf0d9aa2702a0d10d7ce
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2021-08-05 19:47:54 +0200

    security: add lastsfm urls to white list

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 module.exports = {
   target: 'serverless',
-  images: { domains: ['res.cloudinary.com', 'cloudinary.com'] },
+  images: {
+    domains: [
+      'res.cloudinary.com',
+      'cloudinary.com',
+      'fastly.net',
+      '*.fastly.net',
+      'lastfm.freetls.fastly.net',
+    ],
+  },
 };
