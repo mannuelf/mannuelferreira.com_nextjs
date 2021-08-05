5519e170a7bfeb9211556f93fdc156b69a641677
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2021-08-05 19:42:25 +0200

    tests: update env vars

--- __tests__/env.ts
+++ __tests__/env.ts
@@ -1,6 +1,6 @@
 describe('Environment variables', () => {
   it('Should load environment variables from .env', () => {
-    expect(process.env.NEXT_PUBLIC_API_MOCKING).toBe('enabled');
+    expect(process.env.API_MOCKING).toBe('enabled');
   });
 });
 
