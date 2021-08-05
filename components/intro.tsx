0e578db026f4dcb9b0256589d0e1d66bb105da70
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2021-08-05 19:43:33 +0200

    build: update copy

--- components/intro.tsx
+++ components/intro.tsx
@@ -1,11 +1,9 @@
 const Intro = () => {
   return (
     <section className='flex flex-col md:flex-col md:justify-between mt-16 mb-16 md:mb-12 max-w-screen-md'>
-      <h1 className='text-5xl font-bold leading-13 pb-5'>
-        Hello world, <br /> my name is Mannuel Ferreira
-      </h1>
+      <h1 className='text-5xl font-bold leading-13 pb-5'>Hello world.</h1>
       <p className='text-2xl leading-10'>
-        I&apos;m a Software Engineer, I teach Front End development at
+        I&apos;m a Software Engineer teaching Front End development at
         <a
           href='https://www.noroff.no/'
           target='_blank'
@@ -13,7 +11,7 @@ const Intro = () => {
         >
           Noroff Fagskole <i className='fa fa-external-link-alt text-sm'></i>
         </a>
-        , previously I&apos;d spent my days building and maintaining{' '}
+        , previously I helped build, maintain{' '}
         <a
           href='https://superbalist.com'
           target='_blank'
@@ -21,17 +19,17 @@ const Intro = () => {
         >
           Superbalist.com <i className='fa fa-external-link-alt text-sm'></i>
         </a>{' '}
-        a fashion e-commerce store in South Africa.
+        a fashion retailer in Cape Town South Africa.
       </p>
       <p>
         Designing and building web applications and websites is my favourite
-        thing. These days my stack is JavaScript, TypeScript, React, GraphQL
-        (client and servers), Node.js<sup>®</sup>.
+        thing. Currently my tech interests are JavaScript, TypeScript, React,
+        GraphQL (client and servers), Node.js<sup>®</sup>.
       </p>
       <p>
-        I&apos;ve also written websites and API&apos;s in Vue.js, PHP, Laravel,
-        python. I am experienced with Docker, Kubernetes, Linux, AWS, Google
-        Cloud, Heroku, Netlify &amp; Vercel.
+        I&apos;ve also written in Vue.js, PHP, Laravel, python. I am experienced
+        with Docker, Kubernetes, Linux, AWS, Google Cloud, Heroku, Netlify &amp;
+        Vercel.
       </p>
       <p>Looking ahead I am quite interested in React Native and DENO.</p>
     </section>
