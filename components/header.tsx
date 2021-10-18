a905f0c2c1b85d6d6272638efadd8be056833c2e
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2021-10-18 06:44:18 +0200

    tests: adding cypress

--- components/header.tsx
+++ components/header.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Logo from '@components/Logo';
 import Container from '@components/container';
+import Link from 'next/link';
 
 const Header = () => (
   <header className='bg-purple top-0 flex-none flex z-10'>
