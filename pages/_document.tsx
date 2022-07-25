128d54204a7aff6961bee1d69deef0169fb8b4ec
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2022-07-25 13:12:11 +0200

    move script to footer, remove babelrc no longer needed

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
 export default class MyDocument extends Document {
   render() {
@@ -10,14 +11,15 @@ export default class MyDocument extends Document {
             href='https://fonts.googleapis.com/css2?family=Fira+Sans:wght@400;500&family=Ubuntu:wght@300;400;500;700&Ubuntu+Mono:wght@300;400;700&display=swap'
             rel='stylesheet'
           />
-          <script
-            async
-            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-          />
         </Head>
         <body>
           <Main />
           <NextScript />
+          <Script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+            strategy='afterInteractive'
+          />
         </body>
       </Html>
     );
