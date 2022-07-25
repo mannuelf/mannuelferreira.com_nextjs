eb7e28db7f4f78fe76a557c3364a6de46bb44c44
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2022-07-25 18:13:53 +0200

    cleaning house, remove jest and cypress

--- __tests__/env.ts
+++
@@ -1,7 +0,0 @@
-describe('Environment variables', () => {
-  it('Should load environment variables from .env', () => {
-    expect(process.env.API_MOCKING).toBe('enabled');
-  });
-});
-
-export {};
--- test.env.ts
+++
@@ -1,9 +0,0 @@
-import { loadEnvConfig } from '@next/env';
-import { resolve } from 'path';
-
-const testEnv = async () => {
-  const envFile = resolve(__dirname, '.');
-  loadEnvConfig(envFile);
-};
-
-export default testEnv;
