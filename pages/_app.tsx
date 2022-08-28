d0294be3ebba50693e336d78e783c99296cca5f1
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2022-08-28 13:05:12 +0200

    tidyup centralise that to index or route route _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,11 @@ import { AppProps } from 'next/app';
 import '../public/css/tailwind.min.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import * as ga from '@lib/ga';
+import LogRocket from 'logrocket';
+
+if (typeof window !== 'undefined' && process.env.NODE_ENV !== 'development') {
+  LogRocket.init(`${process.env.NEXT_PUBLIC_LOG_ROCKET_ID}/mannueferreiracom`);
+}
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
