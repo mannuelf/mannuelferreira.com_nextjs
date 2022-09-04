6931c0aa4a481126d2c5cec8c61902fa18348b93
Mannuel Ferreira <mannuelf@users.noreply.github.com>
2022-09-04 16:47:28 +0200

    leading forwardslash

--- components/nav.data.ts
+++ components/nav.data.ts
@@ -2,9 +2,9 @@ import {MenuItems} from "./nav.types";
 
 const menuItems: MenuItems = [
   { href: '/', text: 'Home', enabled: true },
-  { href: 'posts', text: 'Posts', enabled: true },
-  { href: 'experience', text: 'Experience', enabled: true },
-  { href: 'books', text: 'Books', enabled: false },
+  { href: '/posts', text: 'Posts', enabled: true },
+  { href: '/experience', text: 'Experience', enabled: true },
+  { href: '/books', text: 'Books', enabled: false },
 ].filter(menu =>  menu.enabled);
 
 
