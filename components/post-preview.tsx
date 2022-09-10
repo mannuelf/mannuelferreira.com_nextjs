00af9e97efbc01290e621f5d3026b29ffb9d3fad
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2022-09-10 07:42:05 +0200

    remove heading and use semantic tag

--- components/post-preview.tsx
+++ components/post-preview.tsx
@@ -11,17 +11,17 @@ type Props = {
 
 const PostPreview = ({ title, date, excerpt, author, slug }: Props) => {
   return (
-    <div className='p-5 m-0 bg-white border-2 border-white hover:border-l-purple rounded-md transition-all'>
+    <article className='p-5 m-0 bg-white border-2 border-white hover:border-l-purple rounded-md transition-all'>
       <Link as={`/posts/${slug}`} href='/posts/[slug]'>
         <a className='hover:text-purple'>
-          <h3 className='mb-3 text-3xl font-medium leading-snug'>{title}</h3>
-          <div className='mb-4 text-lg text-gray-600'>
+          <span className='mb-3 text-3xl font-medium leading-snug'>{title}</span>
+          <span className='block mb-4 text-lg text-gray-600'>
             <DateFormatter dateString={date} />
-          </div>
+          </span>
           <p className='mb-1 text-lg leading-relaxed'>{excerpt}</p>
         </a>
       </Link>
-    </div>
+    </article>
   );
 };
 
