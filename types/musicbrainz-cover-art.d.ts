049fcdc20af3dc89abe1ec0e49bbd1cf4bc1eaf2
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2022-10-06 07:13:05 +0200

    adds music brainz cover art types

---
+++ types/musicbrainz-cover-art.d.ts
@@ -0,0 +1,26 @@
+declare module MusicBainzCoverArt {
+  export interface Thumbnails {
+    1200: string;
+    250: string;
+    500: string;
+    large: string;
+    small: string;
+  }
+
+  export interface Image {
+    approved: boolean;
+    back: boolean;
+    comment: string;
+    edit: number;
+    front: boolean;
+    id: any;
+    image: string;
+    thumbnails: Thumbnails;
+    types: string[];
+  }
+
+  export interface RootObject {
+    images: Image[];
+    release: string;
+  }
+}
