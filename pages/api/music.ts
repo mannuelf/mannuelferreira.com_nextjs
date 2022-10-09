9532372f287a7d2a3a54d98caf7914d7bb1ed61a
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2022-10-09 22:23:41 +0200

    remove old images

--- pages/api/music.ts
+++ pages/api/music.ts
@@ -1,12 +1,8 @@
 import axios from 'axios';
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { ARTIST_ENDPOINT } from '@lib/api/lastFm';
-import artistImages from '@lib/api/artistImages';
 
-const handler = async (
-  req: NextApiRequest,
-  res: NextApiResponse,
-): Promise<void> => {
+const handler = async (req: NextApiRequest, res: NextApiResponse): Promise<void> => {
   if (req.method === 'GET') {
     const data = await getTopArtists();
     res.status(200).json(data);
@@ -17,7 +13,6 @@ export const getTopArtists = async (): Promise<TopArtistsResponse> => {
   try {
     const response = axios({ url: ARTIST_ENDPOINT, method: 'GET' });
     const { data } = await response;
-    data['images'] = artistImages;
     return data;
   } catch (error) {
     throw new Error(`${error}`);
