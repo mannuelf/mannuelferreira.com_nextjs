ac53bdfb26ec05fba5b0ebedc459c9ac4c4baf59
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2022-10-25 20:36:02 +0200

    ok ok

--- pages/scrobbles.tsx
+++ pages/scrobbles.tsx
@@ -98,7 +98,7 @@ const Scrobbles = ({ error, recentTracks, topArtists, userProfile, weeklyAlbumCh
         </div>
       </Container>
       <div className='container mx-auto'>
-        <div>
+        <div className=''>
           {isError.length > 0 ? <div>{error}</div> : null}
           <div className='pb-2 pl-4'>
             <h2 className='text-2xl font-medium'>Recent Tracks 🎹</h2>
