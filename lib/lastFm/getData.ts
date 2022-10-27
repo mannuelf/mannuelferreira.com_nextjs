f7012f1b2efb5d714b2b155e981a70cca9f9b4eb
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2022-10-27 07:22:06 +0200

    finally

--- lib/lastFm/getData.ts
+++ lib/lastFm/getData.ts
@@ -4,5 +4,5 @@ export const getData = async (url: string) =>
   await fetch(`${url}`)
     .then((res) => res.json())
     .then((json) => json)
-    .catch((error) => console.log('🔥', error))
-    .finally(() => {});
+    .catch((error) => console.log('🔥 Uh oh...', error))
+    .finally(() => console.log('🏁 Finished.'));
