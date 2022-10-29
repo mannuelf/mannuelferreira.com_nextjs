4fc8d58a6fb95452e9467e59bb39f43fd85e51d6
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2022-10-29 13:52:02 +0200

    tidy up deps and stuff

--- components/experience-card.tsx
+++ components/experience-card.tsx
@@ -8,18 +8,11 @@ const ExperienceCard = ({ title, date, logo }: ExperienceCardProps) => {
       <div className='p-4 bg-white rounded'>
         <h2 className='pb-1 text-xl font-light'>{title}</h2>
         <h3 className='pb-4 text-2xl font-light'>{date}</h3>
-        <Image
-          width={logo.width}
-          height={logo.height}
-          objectFit='cover'
-          src={logo.href}
-          alt={logo.title}
-        />
+        <Image width={logo.width} height={logo.height} objectFit='cover' src={logo.href} alt={logo.title} />
         <div className='pt-4'>
           <Link href={logo.weblink}>
             <a target='_blank'>
-              {logo.title}{' '}
-              <i className='text-sm fa-solid fa-arrow-up-right-from-square'></i>
+              {logo.title} <i className='text-sm fa-solid fa-arrow-up-right-from-square'></i>
             </a>
           </Link>
         </div>
--- lib/lastFm/fetchData.ts
+++ lib/lastFm/fetchData.ts
@@ -1,10 +1,9 @@
-import fetch, { Response } from 'node-fetch';
+import fetch from 'node-fetch';
 
 const fetchData = async (url: string) =>
   await fetch(`${url}`)
     .then((res) => res.json())
     .then((json) => json)
-    .catch((error) => console.log('🔥 Uh oh...', error))
-    .finally(() => console.log('🏁 Finished.'));
+    .catch((error) => console.log('🔥 Uh oh...', error));
 
 export default fetchData;
