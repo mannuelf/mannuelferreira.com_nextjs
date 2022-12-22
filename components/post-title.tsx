72c07a37d66f9fad009db804557969d5e60dc65a
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2022-12-22 13:00:05 +0100

    make smaller header for desktop

--- components/post-title.tsx
+++ components/post-title.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 const PostTitle = ({ children }: Props) => {
   const styles =
-    'text-4xl md:text-5xl lg:text-6xl font-bold tracking-tight leading-tight md:leading-none mb-3 text-left';
+    'text-4xl md:text-5xl font-bold tracking-tight leading-tight md:leading-none mb-3 text-left';
   return <h1 className={styles}>{children}</h1>;
 };
 
