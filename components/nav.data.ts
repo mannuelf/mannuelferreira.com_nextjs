b5a21adf43a89f8e26aa9ee2c8ebbde18072a626
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2023-02-18 22:54:24 +0100

    adds new ISBNS

--- components/nav.data.ts
+++ components/nav.data.ts
@@ -2,9 +2,9 @@ import {MenuItems} from "./nav.types";
 
 const menuItems: MenuItems = [
   { href: '/', text: 'Home', enabled: true },
-  { href: '/posts', text: 'My posts', enabled: true },
-  { href: '/experience', text: 'My experience', enabled: true },
-  { href: '/books', text: 'Reading', enabled: true },
+  { href: '/posts', text: '📒 Posts', enabled: true },
+  { href: '/experience', text: '💻 Experience', enabled: true },
+  { href: '/books', text: '📚 Books', enabled: true },
 ].filter(menu =>  menu.enabled);
 
 
